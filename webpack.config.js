08400ae5c3c8ecabaee5b7e0028569a6308a6d93
CarlosShi <32117692+carlosshilei@users.noreply.github.com>
2017-09-20 14:37:47 +0800

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  entry: './javascripts/app.js',
+  output: {
+    filename: 'dist/app.js'
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env']
+          }
+        }
+      }
+    ]
+  }
+}
